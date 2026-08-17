add01e439233fbbf987ef41c2320beecb4961d9c
gopalmasulkar <gopalmasulkar2@gmail.com>
2026-08-17 07:30:55 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,78 @@
+class Solution {
+    /*
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int n =temperatures.length;
+        int[] ans = new int[n];
+        for(int i = 0; i< n; i++){
+          
+        
+            for(int  j =i+1; j< n; j++){
+               if(temperatures[j] > temperatures[i]){
+                   ans[i] = j-i;
+                   break;
+                   
+               }
+              
+            }
+        
+
+        }
+        return ans;
+    }
+    */
+
+/*
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> st = new Stack<>();
+        int n =temperatures.length;
+        int[] ans = new int[n];
+        for(int i = 0; i< n; i++){}
+
+        return ans;
+        }
+}
+*/
+
+
+ public int[] dailyTemperatures(int[] temperatures) {
+    int n = temperatures.length;
+
+    int[] ans = new int[n];
+    for(int i =0 ; i<n ;i++){
+        int count=0;
+        boolean flag = false;;
+        for(int j = i+1; j< n; j++){
+            count++;
+             if(temperatures[j] > temperatures[i]){
+                flag = true;
+                break;
+             }
+            //  }else if(temperatures[j] > temperatures[])
+        }
+     ans[i] = (flag)?count:0;
+    }
+    return ans;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
