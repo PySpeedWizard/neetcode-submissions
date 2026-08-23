0633fcbab0d1c96760cbbe94b13c921eae096bae
gopalmasulkar <gopalmasulkar2@gmail.com>
2026-08-23 15:58:46 +0530

    Add: reorder-linked-list - submission-2

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-2.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+public ListNode reverseList(ListNode head) {
+        if(head == null ||head.next == null) return head;
+        ListNode temp = head;
+        ListNode prev = null;
+        while(temp != null){
+            ListNode second = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = second;
+            
+        }
+        return prev;
+    }
+    public void reorderList(ListNode head) {
+        if(head == null || head.next == null)return ;
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode slowNext = reverseList(slow.next);
+        slow.next = null;
+        slow = head;
+        while(slow != null && slowNext != null){
+            ListNode slowfront = slow.next;
+            ListNode slowNextfront = slowNext.next;
+
+              
+            temp.next = slow;
+            slow.next = slowNext;
+
+
+
+            temp = slowNext;
+
+            slow = slowfront;
+            slowNext = slowNextfront;
+            
+
+
+        }
+        if(slow != null){
+            temp.next = slow;
+        }
+        
+    }
+}
